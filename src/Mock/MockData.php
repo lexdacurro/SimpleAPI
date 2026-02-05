f786e65238dc67deed74c177c98938ce802ec313
Alexe Dacurro <lexdacurro@gmail.com>
2026-02-05 01:53:04 -0500

    remove MockData

--- src/Mock/MockData.php
+++ src/Mock/MockData.php
@@ -1,9 +1,8 @@
 <?php 
     namespace SimpleApi\Mock;
     class MockData{
-        public $mockData = ["Greetings" => "Hello"];
         public function generateMock($input){
-            return $this->mockData;
+            // coming soon 
         }
     }
 ?>
